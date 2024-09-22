7daecc81cb8ffa4c337e5cb8bf69c32ca063b81d
adrsha <adrsha@github.com>
2024-09-22 21:36:50 +0545

    removed colors.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,3 @@
-colorList = require("colors")
 return {
   "catppuccin/nvim",
 
@@ -38,7 +37,35 @@ return {
         operators = {},
       },
       color_overrides = {
-        mocha = colorList.colors,
+        mocha = {
+          rosewater = "#e8dee9",
+          flamingo = "#81a1c1",
+          mauve = "#b48ead",
+          pink = "#F5C2E7",
+          red = "#bf616a",
+          maroon = "#E8A2AF",
+          peach = "#d08770",
+          yellow = "#ebcb8b",
+          green = "#a3be8c",
+          blue = "#88c0d0",
+          sky = "#5e81ac",
+          teal = "#B5E8E0",
+          lavender = "#81a1c1",
+
+          text = "#eceff4",
+          subtext1 = "#e5e9f0",
+          subtext0 = "#e5e9f0",
+          overlay2 = "#d8dee9",
+          overlay1 = "#d8dee9",
+          overlay0 = "#4c566a",
+          surface2 = "#434c5e",
+          surface1 = "#3b4252",
+          surface0 = "#2e3440",
+
+          base = "#242933",
+          mantle = "#242933",
+          crust = "#2e3440",
+        }
       },
       custom_highlights = {},
       integrations = {
